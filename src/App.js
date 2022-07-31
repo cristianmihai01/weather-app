d5925d8fc804a7a473c7463907f51eee1f88480f
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-07-31 10:03:11 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+// import axios
+import axios from 'axios';
 // import images
 import ClearImg from './assets/clear.png';
 import CloudsImg from './assets/clouds.png';
@@ -7,12 +9,12 @@ import DrizzleImg from './assets/drizzle.png';
 import RainImg from './assets/rain.png';
 import SnowImg from './assets/snow.png';
 import ThunderstormImg from './assets/thunderstorm.png';
-// import axios
-import axios from 'axios';
-// api key
-const APIkey = 'bcf2048bc3be154bded8f277f580ba2e';
+// import icons
+import { BiSearchAlt } from 'react-icons/bi';
 
 const App = () => {
+  // api key
+  const APIkey = 'bcf2048bc3be154bded8f277f580ba2e';
   const [data, setData] = useState(null);
   const [location, setLocation] = useState('Bucharest');
   const [inputValue, setInputValue] = useState('');
@@ -34,7 +36,7 @@ const App = () => {
       .then((res) => {
         setTimeout(() => {
           setData(res.data);
-        }, 500);
+        }, 1000);
       })
       .catch((err) => {
         setError(err);
@@ -81,15 +83,22 @@ const App = () => {
     <div className='flex h-screen justify-center items-center px-8 xl:px-0'>
       <div className='w-full max-w-[1440px] h-[90vh] p-16 mx-auto bg-gradient-to-tl from-[#4D58DB] via-[#564ACD] to-[#6466DB] rounded-[40px]'>
         {/* form */}
-        <form>
-          <input
-            onChange={(e) => setInputValue(e.target.value)}
-            type='text'
-            placeholder='Enter city name'
-          />
-          <button onClick={(e) => handleSubmit(e)} type='submit'>
-            Search
-          </button>
+        <form className='bg-white h-16 max-w-[358px] mx-auto rounded-full'>
+          <div className='flex h-full justify-between p-1'>
+            <input
+              className='flex-1 bg-transparent border-none outline-none pl-6'
+              onChange={(e) => setInputValue(e.target.value)}
+              type='text'
+              placeholder='Enter city name'
+            />
+            <button
+              className='bg-[#5348DE] w-20 rounded-full'
+              onClick={(e) => handleSubmit(e)}
+              type='submit'
+            >
+              <BiSearchAlt />
+            </button>
+          </div>
         </form>
         <div className='max-w-[420px] mx-auto text-white'>
           <h2 className='text-6xl font-extrabold drop-shadow-lg'>
